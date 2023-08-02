a515e3df34fb3436cf393a3d0245d872aab82714
Ping <roselinepli@gmail.com>
2023-08-02 10:46:59 -0400

    section 25 complete

--- 25.EJS/passingData/index.js
+++ 25.EJS/passingData/index.js
@@ -10,7 +10,10 @@ app.get("/", (req, res) => {
   res.render("index.ejs");
 });
 
-app.post("/submit", (req, res) => {});
+app.post("/submit", (req, res) => {
+  const numLetters = req.body["fName"].length + req.body["lName"].length;
+  res.render("index.ejs", {numberOfLetters:numLetters})
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
